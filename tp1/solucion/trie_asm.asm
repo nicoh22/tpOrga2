2b911b06ef8c29c310ea8541aec70f8ebf5d594b
nicoh22 <nicoh22@hotmail.com>
2014-04-06 16:55:35 -0300

    gran avance en trieBorrar

--- tp1/solucion/trie_asm.asm
+++ tp1/solucion/trie_asm.asm
@@ -35,7 +35,8 @@ extern free
 %define TRUE 1
 
 section .rodata
-
+false: db FALSE
+true: db TRUE
 section .data
 
 section .text
@@ -56,7 +57,50 @@ trie_crear:
 	
 trie_borrar:
 	; COMPLETAR AQUI EL CODIGO
-
+	PUSH RBP
+	MOV RBP RSP
+	PUSH RDI
+	PUSH RBX
+	PUSH R12
+	PUSH R13
+	MOV RBX [RDI]
+	CMP RBX NULL
+	JZ .fin
+	
+	MOV RDI RBX
+	MOV R12 [RDI + offset_sig]
+	MOV R13 [RDI + offset_hijos]
+	CALL free ; Alineada
+	PUSH NULL
+	SUB RSP 8
+.siguiente:
+	CMP R12 NULL
+	JZ .borroHijos
+	PUSH R12	; Desalineada
+	SUB RSP 8 
+.borroHijos:	
+	CMP R13 NULL
+	JZ .rama
+	MOV RDI R13
+	MOV R12 [RDI + offset_sig]
+	MOV R13 [RDI + offset_hijos]
+	CALL free
+	JMP .siguiente
+.rama:
+	ADD RSP 8
+	POP R12
+	CMP R12 NULL
+	JZ .fin
+	
+.fin:
+	MOV RDI [RBP - 8]
+	CALL free
+	POP R13
+	POP R12
+	POP RBX
+	POP RBP
+	RET
+	
 nodo_crear:
 ; nodo * nodo_crear (char c)
 	PUSH RBP
@@ -72,7 +116,7 @@ nodo_crear:
 	mov [rax + offset_sig] NULL
 	mov [rax + offset_hijos] NULL
 	mov [rax + offset_c] BL
-	mov [rax + offset_fin] FALSE
+	mov [rax + offset_fin] false
 	
 	
 	ADD RSP 8
@@ -84,8 +128,20 @@ insertar_nodo_en_nivel:
 	; COMPLETAR AQUI EL CODIGO
 
 trie_agregar_palabra:
-	; COMPLETAR AQUI EL CODIGO
+	; void trie_agregar_palabra(trie *t, char *p)
+	;RDI trie RSI char*
+	PUSH RBP
+	MOV RBP RSP
+	PUSH RBX
+	MOV RBX [RDI]
+	CMP RBX NULL
+	je
+.null:
 
+
+.notnull:	
+	
+	
 trie_construir:
 	; COMPLETAR AQUI EL CODIGO
 
@@ -100,4 +156,15 @@ trie_pesar:
 
 palabras_con_prefijo:
 	; COMPLETAR AQUI EL CODIGO
+	
+;AUX
+
+nodo_borrar:
+	PUSH RBP
+	MOV RBP RSP
+	
+	CALL free
+	
+	POP RBP
+	RET
 
